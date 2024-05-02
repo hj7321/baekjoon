b682379c71f028caf34b1b7fab1477f2cdbf1ee0
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-03 07:50:24 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,13 @@
+function solution(number) {
+  let answer = 0;
+  let n = number.length;
+  for(let i = 0; i < n; i++) {
+    for(let j = i+1; j < n; j++) {
+      for(let k = j+1; k < n; k++) {
+        let sum = number[i] + number[j];
+        if(sum + number[k] === 0) answer++;
+      }
+    }
+  }
+  return answer;
+}
