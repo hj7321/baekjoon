bced5e3707ba19a492cea219ca60f64db2d61383
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-03 08:12:08 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,10 +1,8 @@
 function solution(t, p) {
-    let answer = 0;
-    // t를 숫자 하나 하나를 원소로 가지는 배열로 만들까?
-    // 아니면 t를 숫자 길이만큼 for문?
-    for(let i = 0; i < t.length; i++) {
-        let str = t.slice(i, i+(p.length));
-        if (str.length === p.length && Number(str) <= Number(p)) answer++;
-    }
-    return answer;
+  let answer = 0;
+  for (let i = 0; i <= t.length - p.length; i++) {
+    let str = t.slice(i, i + p.length);
+    if (Number(str) <= Number(p)) answer++;
+  }
+  return answer;
 }
