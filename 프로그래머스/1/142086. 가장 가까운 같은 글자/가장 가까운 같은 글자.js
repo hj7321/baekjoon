f02febc3fdacbb4e1c2ceed3b022d51ae4180764
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-07 08:56:44 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.37 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,9 +1,19 @@
+// function solution(s) {
+//   let splittedStr = s.split("");
+//   let answer = splittedStr.map((cur, idx) => {
+//     let newArr = splittedStr.slice(0, idx).reverse();
+//     let index = newArr.indexOf(cur);
+//     return index === -1 ? -1 : index + 1;
+//   });
+//   return answer;
+// }
+
 function solution(s) {
-  let splittedStr = s.split("");
-  let answer = splittedStr.map((cur, idx) => {
-    let newArr = splittedStr.slice(0, idx).reverse();
-    let index = newArr.indexOf(cur);
-    return index === -1 ? -1 : index + 1;
+  const hash = {};
+
+  return [...s].map((v, i) => {
+    let result = hash[v] !== undefined ? i - hash[v] : -1;
+    hash[v] = i;
+    return result;
   });
-  return answer;
 }
