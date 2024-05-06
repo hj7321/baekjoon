aad3f9b8c82cab55029be93d4c2d6806c94f94ab
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-06 15:39:08 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,6 @@
 function solution(array, commands) {
   return commands.reduce((acc, cur, idx) => {
-    let arr = array
-      .slice(cur[0] - 1, cur[1])
-      .sort((a, b) => (a < b ? -1 : 1));
+    let arr = array.slice(cur[0] - 1, cur[1]).sort((a, b) => a - b);
     acc[idx] = arr[cur[2] - 1];
     return acc;
   }, []);
