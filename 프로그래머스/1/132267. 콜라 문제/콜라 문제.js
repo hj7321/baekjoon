d7582a40fb0338a8e01da28737a60669aae4fac5
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-09 08:05:15 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,22 +1,24 @@
-function solution(a, b, n) {
-  let refrigerator = 0;
-  let sum = 0;
-  const divide = a / b;
-  if (n % a) {
-    refrigerator += n % a;
-    n = n - (n % a);
-  }
-  while (true) {
-    n = Math.floor(n / divide);
-    sum += n;
-    refrigerator += n;
-    if (refrigerator < a) break;
-    if (refrigerator % a) {
-      n = refrigerator - (refrigerator % a);
-    } else {
-      n = refrigerator;
-    }
-    refrigerator -= n;
-  }
-  return sum;
-}
+// function solution(a, b, n) {
+//   let refrigerator = 0;
+//   let sum = 0;
+//   const divide = a / b;
+//   if (n % a) {
+//     refrigerator += n % a;
+//     n = n - (n % a);
+//   }
+//   while (true) {
+//     n = Math.floor(n / divide);
+//     sum += n;
+//     refrigerator += n;
+//     if (refrigerator < a) break;
+//     if (refrigerator % a) {
+//       n = refrigerator - (refrigerator % a);
+//     } else {
+//       n = refrigerator;
+//     }
+//     refrigerator -= n;
+//   }
+//   return sum;
+// }
+
+solution = (a, b, n) => Math.floor((n - b) / (a - b)) * b;
