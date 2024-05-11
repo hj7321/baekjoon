24fe6fa91e210267778e79d127b93c201c29cc37
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-11 13:24:31 +0900

    [level 1] Title: 덧칠하기, Time: 1.71 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,12 @@
+function solution(n, m, section) {
+  let answer = 1;
+  let end = section[0] + (m - 1);
+  for (let i = 1; i < section.length; i++) {
+    if (end >= section[i]) continue;
+    else {
+      end = section[i] + (m - 1);
+      answer++;
+    }
+  }
+  return answer;
+}
