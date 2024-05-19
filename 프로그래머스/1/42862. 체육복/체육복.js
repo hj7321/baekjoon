209a5c9dc715d05093fce368d6345edc24f8a1c0
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-19 12:33:42 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,25 +1,43 @@
+// function solution(n, lost, reserve) {
+//   let resultCnt = 0;
+//   const lostCopy = [...lost];
+//   resultCnt += lost.reduce((count, curNum) => {
+//     if (reserve.indexOf(curNum) !== -1) {
+//       reserve.splice(reserve.indexOf(curNum), 1);
+//       lostCopy.splice(lostCopy.indexOf(curNum), 1);
+//       ++count;
+//     }
+//     return count;
+//   }, 0);
+//   resultCnt += lostCopy
+//     .sort((a, b) => a - b)
+//     .reduce((count, curNum) => {
+//       if (reserve.indexOf(curNum - 1) !== -1) {
+//         ++count;
+//         reserve.splice(reserve.indexOf(curNum - 1), 1);
+//       } else if (reserve.indexOf(curNum + 1) !== -1) {
+//         ++count;
+//         reserve.splice(reserve.indexOf(curNum + 1), 1);
+//       }
+//       return count;
+//     }, 0);
+//   return n - lost.length + resultCnt;
+// }
+
 function solution(n, lost, reserve) {
-  let resultCnt = 0;
-  const lostCopy = [...lost];
-  resultCnt += lost.reduce((count, curNum) => {
-    if (reserve.indexOf(curNum) !== -1) {
-      reserve.splice(reserve.indexOf(curNum), 1);
-      lostCopy.splice(lostCopy.indexOf(curNum), 1);
-      ++count;
-    }
-    return count;
-  }, 0);
-  resultCnt += lostCopy
-    .sort((a, b) => a - b)
-    .reduce((count, curNum) => {
-      if (reserve.indexOf(curNum - 1) !== -1) {
-        ++count;
-        reserve.splice(reserve.indexOf(curNum - 1), 1);
-      } else if (reserve.indexOf(curNum + 1) !== -1) {
-        ++count;
-        reserve.splice(reserve.indexOf(curNum + 1), 1);
-      }
-      return count;
-    }, 0);
-  return n - lost.length + resultCnt;
+  let reserveCopy = reserve
+    .filter((r) => !lost.includes(r))
+    .sort((a, b) => a - b);
+  const lostCopy = lost
+    .filter((l) => !reserve.includes(l))
+    .sort((a, b) => a - b);
+
+  return (
+    n -
+    lostCopy.filter((a) => {
+      const b = reserveCopy.find((r) => Math.abs(r - a) === 1);
+      if (!b) return true;
+      reserveCopy = reserveCopy.filter((r) => r !== b);
+    }).length
+  );
 }
