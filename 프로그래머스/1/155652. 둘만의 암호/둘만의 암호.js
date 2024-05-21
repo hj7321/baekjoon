dfd39c74286d9d1febeb768bd7b74b50cc6fb717
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-21 10:24:29 +0900

    [level 1] Title: 둘만의 암호, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,16 +1,23 @@
+// function solution(s, skip, index) {
+//   const skipArr = skip.split("").map((ele) => ele.charCodeAt());
+//   const sArr = s.split("").map((ele) => ele.charCodeAt());
+//   const resultArr = sArr.map((ele) => {
+//     for (let i = 1; i <= index; i++) {
+//       ++ele;
+//       if ("z".charCodeAt() < ele) ele = "a".charCodeAt();
+//       while (skipArr.includes(ele)) {
+//         ++ele;
+//         if ("z".charCodeAt() < ele) ele = "a".charCodeAt();
+//       }
+//     }
+//     return String.fromCharCode(ele);
+//   });
+//   return resultArr.join("");
+// }
+
 function solution(s, skip, index) {
-  const skipArr = skip.split("").map((ele) => ele.charCodeAt());
-  const sArr = s.split("").map((ele) => ele.charCodeAt());
-  const resultArr = sArr.map((ele) => {
-    for (let i = 1; i <= index; i++) {
-      ++ele;
-      if ("z".charCodeAt() < ele) ele = "a".charCodeAt();
-      while (skipArr.includes(ele)) {
-        ++ele;
-        if ("z".charCodeAt() < ele) ele = "a".charCodeAt();
-      }
-    }
-    return String.fromCharCode(ele);
-  });
-  return resultArr.join("");
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+                      "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+                      "u", "v", "w", "x", "y", "z"].filter(c => !skip.includes(c));
+    return s.split("").map(c => alphabet[(alphabet.indexOf(c) + index) % alphabet.length]).join("");
 }
