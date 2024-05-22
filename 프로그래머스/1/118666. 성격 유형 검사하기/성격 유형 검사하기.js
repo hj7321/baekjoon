dcf5329f2b4621ef2997d2b375d45f7328bc6ffd
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-22 10:08:31 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,15 +1,6 @@
 function solution(survey, choices) {
   let answer = "";
-  const obj = {
-    R: 0,
-    T: 0,
-    C: 0,
-    F: 0,
-    J: 0,
-    M: 0,
-    A: 0,
-    N: 0,
-  };
+  const obj = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
 
   choices.forEach((cur, idx) => {
     if (cur < 4) {
