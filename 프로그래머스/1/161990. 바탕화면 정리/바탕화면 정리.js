d0a0bd4300c1e13fea86d3d3eb1b971d9e12cd46
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-05-25 12:53:48 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,16 +1,14 @@
 function solution(wallpaper) {
   const [minRow, maxRow, minCol, maxCol] = wallpaper.reduce(
     ([minRow, maxRow, minCol, maxCol], curStr, idx) => {
-      if (curStr.includes("#")) {
-        if (minRow > idx + 1) minRow = idx + 1;
-        if (maxRow < idx + 1) maxRow = idx + 1;
-        [...curStr].forEach((curChar, index) => {
-          if (curChar === "#") {
-            if (minCol > index + 1) minCol = index + 1;
-            if (maxCol < index + 1) maxCol = index + 1;
-          }
-        });
-      }
+      [...curStr].forEach((curChar, index) => {
+        if (curChar === "#") {
+          if (minRow > idx + 1) minRow = idx + 1;
+          if (maxRow < idx + 1) maxRow = idx + 1;
+          if (minCol > index + 1) minCol = index + 1;
+          if (maxCol < index + 1) maxCol = index + 1;
+        }
+      });
       return [minRow, maxRow, minCol, maxCol];
     },
     [51, 0, 51, 0]
