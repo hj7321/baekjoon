1dfa2b3791637f23e68d243dc33caf2c9e23cb85
hj7321 <117134693+hj7321@users.noreply.github.com>
2024-06-14 08:46:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  const arr = s.split(" ");
+  const newArr = arr.map((cur) => {
+    let newStr = "";
+    for (let i = 0; i < cur.length; i++) {
+      if (typeof cur[i] === "number" || cur[i] === " ") newStr += cur[i];
+      else newStr += i === 0 ? cur[i].toUpperCase() : cur[i].toLowerCase();
+    }
+    return newStr;
+  });
+  return newArr.join(" ");
+}
